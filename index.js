39ace32fae57ab8c6945ad9ad7a3a282a68a1852
jivan85 <jivan0608@gmail.com>
2024-01-21 16:49:20 +0300

    uibuild

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.get('/api/persons/:id', (request, response, next) => {
 app.delete('/api/persons/:id', (request, response, next) => {
   Note.findByIdAndDelete(request.params.id)
     .then(result=> {
-      response.status(204).end
+      response.status(204).end()
     })
     .catch(error=>next(error))
 })
