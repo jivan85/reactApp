d319270301f12b0b61151d484f5ab6b9e214687b
jivan85 <jivan0608@gmail.com>
2024-01-27 15:34:34 +0300

    uibuild

--- models/person.js
+++ models/person.js
@@ -7,7 +7,11 @@ console.log('connecting to', url)
 mongoose.connect(url)
   .then(result => {    console.log('connected to MongoDB')  })  .catch((error) => {    console.log('error connecting to MongoDB:', error.message)  })
 const personSchema = new mongoose.Schema({
-  name: String,
+  name: {
+    type: String,
+    minLength: 3,
+    required: true
+  },
   number: String,
 })
 
